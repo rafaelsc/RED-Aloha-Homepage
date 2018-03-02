e3c96fef4c7099534d9bcce83f1be08c8ab9e4ed
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-03-01 20:44:36 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ $(function(){
         contain: true
     });
 
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 'fast');
+    });
+
+    
+
 })
